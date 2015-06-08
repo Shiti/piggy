d7862b3cb912a5aa3db88838744a798c823afc9b
Shiti <ssaxena.ece@gmail.com>
2015-06-08 22:33:03 +0530

    taking file format in options

--- pgsparky/__init__.py
+++ pgsparky/__init__.py
@@ -12,41 +12,49 @@ class SparkSQLWrapper(ForeignDataWrapper):
             log_to_postgres('The master parameter is required and the default is local.', WARNING)
         self.master = options.get("master", "local")
 
+        if 'sourceType' not in options:
+            log_to_postgres('The sourceType parameter is required and the default is jsonFile.', WARNING)
+        self.sourceType = options.get("sourceType", "jsonFile")
+
         try:
             self.file = options.get("file")
         except Exception, e:
             log_to_postgres('The file parameter is required.', ERROR)
 
         if 'table' not in options:
-            log_to_postgres('table or query parameter is required.', ERROR)
+            log_to_postgres('The table parameter is required.', ERROR)
         self.table = options.get("table", None)
 
         self.columns = columns
         self.sc = SparkContext(self.master, "pgSparky")
         self.sqlContext = SQLContext(self.sc)
-        self.data = self.sqlContext.jsonFile(self.file)
+
+        if self.sourceType == 'jsonFile':
+            self.data = self.sqlContext.jsonFile(self.file)
+        elif self.sourceType == 'parquetFile':
+            self.data = self.sqlContext.parquetFile(self.file)
+        else:
+            log_to_postgres('jsonFile and parquetFile are the only supported types.', ERROR)
+
         self.sqlContext.registerDataFrameAsTable(self.data, self.table)
 
     def execute(self, quals, columns):
-        log_to_postgres(quals, DEBUG)
-
-        def qualToString(qual):
-            field = qual.field_name
-            oper = qual.operator
+        def qual_to_string(qual):
             value = qual.value
             if isinstance(value, basestring):
-                return field + oper + " '" + str(value) + "'"
+                return str(qual).replace(value, "'"+value+"'")
             else:
-                return field + oper + str(value)
+                return str(qual)
 
         query = "SELECT " + ",".join(self.columns.keys()) + " FROM " + self.table
-        whereClause = " AND ".join(map(str, quals))
+        where_clause = " AND ".join(map(qual_to_string, quals))
 
-        if not whereClause:
+        if not where_clause:
             query = query
         else:
-            query = query + " WHERE " + whereClause
+            query = query + " WHERE " + where_clause
 
+        log_to_postgres(query, DEBUG)
         result = self.sqlContext.sql(query)
         for row in result.collect():
             yield row
